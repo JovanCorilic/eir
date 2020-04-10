460ced2d672fc9c0ebc9b721e3f493b866d880c3
sw792017 <52822584+sw792017@users.noreply.github.com>
2020-04-10 18:03:10 +0200

    django admin

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Snippet
 
-# Register your models here.
+admin.site.site_header = "Klinika"
+
+
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created')
+    list_filter = ('created',)
+    change_list_template = 'admin/snippets/listapromene.html'
+
+
+admin.site.register(Snippet, SnippetAdmin)
+admin.site.unregister(Group)
